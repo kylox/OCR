b797acbab6e648fb2493fea0ffeea79eb693aff7
kylox <maxime.gaudron@hotmail.fr>
2013-10-02 11:56:51 +0200

    ajout filtre median

--- OCR.ml
+++ OCR.ml
@@ -18,21 +18,27 @@ let show img dst =
     Sdlvideo.blit_surface d dst ();
     Sdlvideo.flip dst
 
-let level (r, g, b) = 0.3 *. float r +. 0.59 *. float g +. 0.11 *.float  b;;
+let level (r, g, b) = (r + g + b)/3;;
 
-let color2green (r,g,b) = 
-  (int_of_float (level(r, g, b) *. 255.) , int_of_float (level(r, g, b) *.
-  255.), int_of_float (level(r, g, b) *. 255.));;
-
-
-let surexposition (r, g, b) =
-  (int_of_float (level(r, g, b) /. 0.3) , int_of_float (level(r, g, b) /. 0.59) , int_of_float (level(r, g, b) /. 0.11));;
+let color2grey (r,g,b) = 
+  (level(r, g, b), level(r, g, b), level(r, g, b));;
 
 let image2grey f img dst  =
   for x = 0 to ((Sdlvideo.surface_info img).Sdlvideo.h-1) do
     for y = 0  to ((Sdlvideo.surface_info img).Sdlvideo.w-1) do
      let (r,g,b) = f (Sdlvideo.get_pixel_color img x y) in
       Sdlvideo.put_pixel_color dst x y (r,g,b)
+    done
+  done
+
+
+let binarisation img dst =
+  for x = 0 to ((Sdlvideo.surface_info img).Sdlvideo.h-1) do
+    for y = 0  to ((Sdlvideo.surface_info img).Sdlvideo.w-1) do
+	let (r,g,b) =  Sdlvideo.get_pixel_color img x y in 
+		if (r+b+g/3) < 127 then (r,g,b) = (0,0,0)
+		else (r,g,b) = (255,255,255);
+		Sdlvideo.put_pixel_color dst x y (r,g,b)
     done
   done
 
@@ -44,8 +50,10 @@ let main () =
     let img = Sdlloader.load_image Sys.argv.(1) in
     let (w,h) = get_dims img in
     let tem = Sdlvideo.create_RGB_surface_format img [] w h in
+    let dst = Sdlvideo.create_RGB_surface_format img [] w h in
     let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] in
-    image2grey color2green img tem; 
+    image2grey color2grey img tem; 
+    binarisation tem dst;
     show tem display;
     wait_key ();
     exit 0
