3f098d6340cf4dca33be9309b812b16a00eae595
Forgetlouis <louis.forget.78@gmail.com>
2013-10-20 16:58:14 +0200

    Binarize qui marche pour toutes les images mais qui pue du zboub

--- pre_treatment/binarize.ml
+++ pre_treatment/binarize.ml
@@ -1,4 +1,4 @@
-let var k h  = 
+ (*let var k h  = 
   let v = ref 0. in
   for i = 0 to k do
     v := !v +. h.(i) *. float i
@@ -48,10 +48,23 @@ let binarize img =
  let (w,h) = Image_helper.get_dims img in
    for i = 0 to w-1 do
      for j =0 to h-1 do
-       print_int(moy);
+     (*  print_int(moy); *)
        if To_grey.get_r(Sdlvideo.get_pixel_color img i j) <= moy then
         Sdlvideo.put_pixel_color img i j (0,0,0)
        else
         Sdlvideo.put_pixel_color img i j (255,255,255)
       done;
-     done;
+     done; *)
+let binarize greypict =
+	let(w,h) = Image_helper.get_dims greypict in
+	  for y=0 to h-1 do
+	     for x=0 to w-1 do
+               let pix = Sdlvideo.get_pixel_color greypict x y in
+                 if To_grey.get_r(pix) <=127 then
+		    Sdlvideo.put_pixel_color greypict x y (0,0,0)
+                 else
+                    Sdlvideo.put_pixel_color greypict x y (255,255,255)
+              done;
+           done;
+
+                
