856985fbe7e8dd91c0150b9cb70f8573adbbc9fe
Forgetlouis <louis.forget.78@gmail.com>
2013-10-23 01:09:30 +0200

    Rotation corrigé

--- pre_treatment/rotation.ml
+++ pre_treatment/rotation.ml
@@ -12,15 +12,15 @@ let hough matrice =
         (* Allez les noobs, on va parcourire l'image *)
         for y=0 to h-1 do
            for x=0 to w-1 do
-              if (matrice.(x).(y) == (0,0,0)) then
+              if (matrice.(x).(y) = (0,0,0)) then
                 begin
              (* t est l angle qu on fera variee -pi/2 a pi/2 *)
                 let t = ref(-.pi02) in
                 (* parcour de l'inertvalle d'angle  *)                   
                  while ( !t <= pi02 ) do
-                  let droite =int_of_float((float x *. (cos !t))+.(float
-                  y *. (sin !t))) in
-                       if droite>=0 then (* on remplis le tableau de vote *)
+                  let droite =int_of_float(((float y) *. (cos !t))+.((float
+                  x) *. (sin !t))) in
+                       if droite >= 0 then (* on remplis le tableau de vote *)
                          begin
                           let teta_i = int_of_float(!t*.100. +. pi02*.100.)in
                            matrice_de_vote.(droite).(teta_i) <-
