76c4cfdeaf29b6a0d703be1f70030ebd5841a0aa
kylox <maxime.gaudron@hotmail.fr>
2013-10-28 11:53:16 +0100

    modification de la distance de lenvshentien op

--- levenshtein.ml
+++ levenshtein.ml
@@ -1,22 +1,26 @@
-let get_distance str1 str2 =
-	if (str1 = str2) then 0
-	else if (str1 = "") then String.length str2
-	else if (str2 = "") then String.length str1
-	else
-		let s1 = if String.length str1 < String.length str2 then str1 else str2 in
-		let s2 = if s1 = str1 then str2 else str1 in
-		let array1 = Array.init (String.length s2 + 1) (fun i -> i) and
-			array2 = Array.make (String.length s2 + 1) 0 in
-				for i = 0 to String.length s1 - 1 do
-					array2.(i) <- i + 1;
-					for j = 0 to String.length s2 - 1 do
-						let cost = if s1.[i] = s2.[j] then 0 else 1 in
-							array2.(j+1) <- min (array2.(j) + 1) (min (array1.(j + 1) + 1) (array1.(j) + cost))
-					done;
-					Array.blit array2 0 array1 0 (Array.length array2)
-				done;
-				array2.(String.length s2);;
+let get_matrix_distance x y =
+  let m = Array.make_matrix ((String.length x)+1) ((String.length) y+1) 0 in
+  let rec distance = function
+      (0,k)-> let x = k in m.(0).(k) <- x;x
+    |(k,0) -> let x = k in m.(k).(0) <- x;x 
+    |(i,j) when x.[i-1] = y.[j-1] -> let x = min ((distance (i-1, j)) +1) (min ((distance (i, j-1)) +1)  (distance (i-1, j-1))) in m.(i).(j) <- x;x
+    |(i,j) -> let x = min ((distance (i - 1, j)) + 1) (min ((distance (i, j - 1)) + 1) ((distance (i-1, j-1)) + 2)) in m.(i).(j) <- x;x
+  in
+    (distance ((String.length x), (String.length y)));
+      m
 
-print_int (get_distance "bite" "vitesse")
-
-(Array.Sub (i * nl.neurons.(i).nb_li) nl.neurons.(i).nb_li)
+let _ = let m = get_matrix_distance "intention" "execution" in
+  print_string("   |0|1|2|3|4|5|6|7|8|9|");
+  print_newline();
+    print_string("--------------------------");
+  for i = 0 to 9 do
+    print_newline();
+    print_string("---------------");
+    print_newline();
+    print_int(i); print_string("||");
+      for j = 0 to 9 do
+        print_string("|");
+        print_int(m.(i).(j))
+      done;
+  done 
+ 
